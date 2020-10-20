ce881c1054bed1b83c7d955a409985f174c831b2
julie9312 <67768114+julie9312@users.noreply.github.com>
2020-10-20 16:48:41 +0900

    1020

---
+++ Photos/app/src/main/java/com/julie/photos/ImgPhoto.java
@@ -0,0 +1,14 @@
+package com.julie.photos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImgPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img_photo);
+    }
+}
---
+++ Photos/app/src/main/java/com/julie/photos/MainActivity.java
@@ -0,0 +1,77 @@
+package com.julie.photos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // volley 에서 사용하는 클래스.
+    RequestQueue requestQueue;
+    ArrayList<Photo> photoArrayList = new ArrayList<>();
+    public static final String URL = "https://jsonplaceholder.typicode.com/photos";
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i("AAA", response.toString());
+                        for(int i = 0; i < response.length(); i++){
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String title = jsonObject.getString("title");
+                                String url = jsonObject.getString("url");
+                                String thumbnailUrl = jsonObject.getString("thumbnailUrl");
+
+                                Photo photo = new Photo(title, url, thumbnailUrl);
+                                photoArrayList.add(photo);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        recyclerViewAdapter =
+                                new RecyclerViewAdapter(MainActivity.this, photoArrayList);
+                        recyclerView.setAdapter(recyclerViewAdapter);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+
+    }
+}
